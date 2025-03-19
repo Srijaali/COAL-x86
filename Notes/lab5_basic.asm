bab3aa8d3e116236f270bf5a7a3cfd8c2117410b
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2025-03-20 03:46:05 +0500

    Update lab5_basic.asm

--- Notes/lab5_basic.asm
+++ Notes/lab5_basic.asm
@@ -110,3 +110,43 @@ main PROC
 
 main endp
 end main
+
+;l5 q7
+INCLUDE Irvine32.inc
+
+.data
+    arrayB BYTE 60, 70, 80        ; BYTE array
+    arrayW WORD 150, 250, 350     ; WORD array
+    arrayD DWORD 600, 1200, 1800  ; DWORD array
+
+.code
+main PROC
+    ; Add first and last element of BYTE array (arrayB)
+    mov esi, OFFSET arrayB      ; ESI points to the start of arrayB
+    movzx eax, byte ptr [esi]   ; Load first element (arrayB[0]) into EAX
+    movzx ebx, byte ptr [esi + 2 * TYPE arrayB] ; Load last element (arrayB[2]) into EBX (using TYPE to calculate offset)
+    add eax, ebx                ; Add first and last element of arrayB
+    call WriteDec               ; Display the result
+    call CrLf                   ; New line
+
+    ; Add first and last element of WORD array (arrayW)
+    mov esi, OFFSET arrayW      ; ESI points to the start of arrayW
+    movzx eax, word ptr [esi]   ; Load first element (arrayW[0]) into EAX
+    movzx ebx, word ptr [esi + 2 * TYPE arrayW] ; Load last element (arrayW[2]) into EBX (using TYPE to calculate offset)
+    add eax, ebx                ; Add first and last element of arrayW
+    call WriteDec               ; Display the result
+    call CrLf                   ; New line
+
+    ; Add first and last element of DWORD array (arrayD)
+    mov esi, OFFSET arrayD      
+    mov eax, [esi]              
+    mov ebx, [esi + 2 * TYPE arrayD] 
+    add eax, ebx                
+    call WriteDec               
+    call CrLf                  
+
+    call DumpRegs               ; Dump the registers for inspection
+    exit
+main ENDP
+
+END main
