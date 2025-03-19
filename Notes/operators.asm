35b4b85cf06e7d25d150967f67218dd05901d178
Syeda Rija Ali <142867637+Srijaali@users.noreply.github.com>
2025-03-20 00:03:05 +0500

    Update operators.asm

--- Notes/operators.asm
+++ Notes/operators.asm
@@ -1,1 +1,13 @@
-amsm
+#assignment op:
+INCLUDE Irvine32.inc
+.data
+	SecondsInDay = 24*60*60
+.code
+main PROC
+	mov eax,SecondsInDay
+	call WriteInt
+	call crlf
+call DumpRegs
+exit
+main ENDP
+END main
